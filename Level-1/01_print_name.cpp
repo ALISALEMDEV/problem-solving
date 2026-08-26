b94f3bebcfd87a0b39b146c4ee4eba3ee4eae19e
Ali Salem Ettorkzi <91334623+ALISALEMDEV@users.noreply.github.com>
2026-08-26 07:28:25 +0100

    Create 01_print_name.cpp

---
+++ Level-1/01_print_name.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void printname(string Name) {
+
+    cout << "\n your name is : " << Name <<endl;
+
+}
+
+int main()
+
+{
+
+    printname("Alisalem");
+
+    return 0;
+}
