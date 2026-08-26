cabcbdd1c81d051b85b8f56e6c9dc8bb2a7ef61c
Ali Salem Ettorkzi <91334623+ALISALEMDEV@users.noreply.github.com>
2026-08-26 07:58:28 +0100

    Add C++ program to sum even numbers from 1 to n

---
+++ Level-1/29_sum_even_numbers_1_to_n.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+enum enOddOrEven { odd = 1, even = 2 };
+int ReadNum() {
+    int n;
+    cout << "enter n : " << endl;
+    cin >> n;
+    return n;
+}
+enOddOrEven CheakOddOrEven(int n) {
+    if (n % 2 != 0)
+        return enOddOrEven::odd;
+    else
+        return enOddOrEven::even;
+}
+int PrintRangeFrom1toN_UsingWhile(int n)
+{
+    int counter = 0;
+    int sum = 0;
+    cout << "sum even numbers by using while loop \n";
+    while (counter < n)
+    {
+        counter++;
+        if (CheakOddOrEven(counter) == enOddOrEven::even)
+            sum += counter;
+    }
+    return sum;
+}
+int PrintRangeFrom1toN_UsingDoWhile(int n) {
+    int counter = 0;
+    int sum = 0;
+    cout << "sum even numbers by using do while loop \n";
+    do
+    {
+        counter++;
+        if (CheakOddOrEven(counter) == enOddOrEven::even)
+            sum += counter;
+    } while (counter < n);
+    return sum;
+}
+int PrintRangeFrom1toN_UsingFor(int n)
+{
+    int sum = 0;
+    cout << "sum even numbers by using do while loop \n";
+    for (int i = n; i >= 1; i--)
+    {
+
+        if (CheakOddOrEven(i) == enOddOrEven::even)
+            sum += i;
+
+    }
+    return sum;
+}
+
+int main()
+{
+    int n = ReadNum();
+    cout << PrintRangeFrom1toN_UsingWhile(n) << endl;
+    cout << PrintRangeFrom1toN_UsingDoWhile(n) << endl;
+    cout << PrintRangeFrom1toN_UsingFor(n) << endl;
+
+}
